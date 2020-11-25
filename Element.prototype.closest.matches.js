fcd879afa56b99f818ef723fe9fcbc58a1fa9cc2
Darren Embry <dse@webonastick.com>
2020-11-25 12:26:31 -0500

    *** empty log message ***

---
+++ Element.prototype.closest.matches.js
@@ -0,0 +1,20 @@
+/**
+ * Element.prototype.closest and Element.prototype.matches polyfills for IE11
+ * https://developer.mozilla.org/en-US/docs/Web/API/Element/closest
+ * with minor modifications for code quality
+ */
+if (!Element.prototype.matches) {
+    Element.prototype.matches = Element.prototype.msMatchesSelector || Element.prototype.webkitMatchesSelector;
+}
+if (!Element.prototype.closest) {
+    Element.prototype.closest = function(s) {
+        var el = this;
+        do {
+            if (Element.prototype.matches.call(el, s)) {
+                return el;
+            }
+            el = el.parentElement || el.parentNode;
+        } while (el !== null && el.nodeType === 1);
+        return null;
+    };
+}
